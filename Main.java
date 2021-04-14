9f816a293554abb95307b7fb6a168aa6665c91a4
Ashok Mane <ashokmane81800@gmail.com>
2021-04-14 17:45:19 +0530

    [ashok] created a java file to calculate employee wage

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("**WELCOME TO eMPLOYEE MANAGEMENT SYSTEM**");
+		
+		Employee employee = new Employee();
+		employee.check();
+	}
+
+}
